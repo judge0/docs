d149105eb143b041bd1da02cd3599dc3b5b62b64
Herman Zvonimir Došilović <hermanz.dosilovic@judge0.com>
2025-11-15 00:46:16 +0100

    Add more theme options in conf.py

--- source/conf.py
+++ source/conf.py
@@ -11,6 +11,21 @@ exclude_patterns = []
 
 html_title = project
 html_theme = 'sphinxawesome_theme'
+html_favicon = "./_static/Judge0 Icon 02 Rounded 1024x1024.png"
+html_theme_options = {
+    "logo_light": "./_static/Judge0 Icon 07 Rounded 1024x1024.png",
+    "logo_dark": "./_static/Judge0 Icon 06 Rounded 1024x1024.png",
+    "main_nav_links": {
+        "Docs": "/index",
+        "Products": "/products",
+        "Pricing": "/products/judge0/pricing",
+        "FAQ": "/faq",
+    },
+    "show_prev_next": True,
+    "awesome_external_links": True,
+    "breadcrumbs_separator": "•",
+    "show_scrolltop": True,
+}
 html_permalinks_icon = Icons.permalinks_icon
 html_static_path = ['_static']
 html_show_sphinx = False
